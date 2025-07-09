982266cdd9c1f6ecd7a3b2ab5853971eaeffda95
Albert Garcia Plaza <gplaza91@gmail.com>
2025-07-09 15:54:57 +0100

    Added base imports from ble.h module

---
+++ src/managers/ble/include/ble.h
@@ -0,0 +1,13 @@
+#ifndef BLE_H
+#define BLE_H
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_event.h"
+#include "esp_flash.h"
+#include "esp_nimble_hci.h"
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+
+#endif
